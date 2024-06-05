22547e4d8a212e6f198a2b76a55ce26b38e6ad5e
ALUNO <ALUNO@10.85.198.2>
2024-06-05 14:09:56 -0300

    Commit project Text04

---
+++ src/ProblemSet2_1.java
@@ -0,0 +1,23 @@
+public class ProblemSet2_1 {
+    public static void main(String[] args) {
+        System.out.println("x  xxxxxxxxxxxxxxxxx  x");
+        System.out.println(" x   xxxx      xxxx  x ");
+        System.out.println("  x    x        x   x  ");
+        System.out.println("   x       xx      x   ");
+        System.out.println("    x  x       x  x    ");
+        System.out.println("     x  x     x  x     ");
+        System.out.println("      x  x x x  x      ");
+        System.out.println("        xxxxxxxx        ");
+    }    
+}
+      
+        
+        
+        
+        
+        
+        
+        
+        
+      
+
